8369b5c0fbe8af1aa2b726e2f4329f832b969f55
Greg0s <gregoire.tinnes@outlook.fr>
2023-03-23 00:25:38 +0100

    clean main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,3 @@ import App from './App.vue'
 
 
 createApp(App).mount('#app')
-
-// let main = {
-//     async getDataset(){
-//         const data = await lastfm.getTopSongs();
-//         let tracks = data.tracks.track;
-//         return tracks;
-//         //console.log(data.tracks.track);
-//         // console.log(data[0]["name"]);
-//         //document.querySelector('#test').innerHTML=tracks[5].name;
-//     },
-//     getRandomSong(songList) {
-//         //let rand = floor(Math.random()*50);
-//         return songList[5];
-//     },
-//     play(song){
-//         QuizCover.setCover(song.image[3]);
-//         QuizAnswerField.setAnswer(song.artist.name);
-//     }
-// }

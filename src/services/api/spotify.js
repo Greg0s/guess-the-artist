1b516509cd567fe83dd5fb09b769532e58f5a49a
Greg0s <gregoire.tinnes@outlook.fr>
2023-03-23 00:25:21 +0100

    automate get bearer token

--- src/services/api/spotify.js
+++ src/services/api/spotify.js
@@ -1,7 +1,22 @@
-import { bearer_spotify } from './api_key.js';
+import { id_spotify } from './api_key.js';
+let bearer_spotify = "";
 
 let spotify = {
+    async getAccessToken(){
+        let res = await fetch('https://accounts.spotify.com/api/token?grant_type=client_credentials', {
+            method: "POST",
+            headers: {
+                "Authorization" : "Basic " + id_spotify,
+                "Content-Type" : "application/x-www-form-urlencoded"
+            }
+        });
+        if(!res) {
+            throw new Error("Something went wrong!");
+        }
+        return res.json();
+    },
     async getSongs(){   
+        if(bearer_spotify == ""){await newToken()}
         let res = await fetch('https://api.spotify.com/v1/playlists/1ISSOeZLHpzuOJ0CdSYwgD/tracks', {
             method: "GET",
             headers: {
@@ -14,6 +29,7 @@ let spotify = {
         return res.json();
     },
     async getArtist(id){
+        if(bearer_spotify == ""){await newToken()}
         let res = await fetch('https://api.spotify.com/v1/artists/' + id, {
             method: "GET",
             headers: {
@@ -26,5 +42,11 @@ let spotify = {
         return res.json();
     }
 }
+
+async function newToken(){
+    bearer_spotify = await spotify.getAccessToken();
+    bearer_spotify = bearer_spotify.access_token;
+}
+setInterval(newToken(), 1000 * 60 * 60);
 
 export default spotify;

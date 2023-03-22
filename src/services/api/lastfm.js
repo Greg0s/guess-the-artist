bda16fbcc51686eb1f5c9d4ecfc936985aa7cede
Greg0s <gregoire.tinnes@outlook.fr>
2023-03-22 21:59:10 +0100

    old files with lastfm/ocr api for archives purpose

--- src/services/api/lastfm.js
+++ src/services/api/lastfm.js
@@ -1,15 +1,29 @@
-const api_key = '70aedb578d041a0913ff55cb0389bc00';
+import { api_key_lastfm } from './api_key.js';
 
 let lastfm = {
-    async getTopSongs(){      
-        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=united states&api_key='+ api_key +'&format=json');
+    async getTopSongs(){   
+        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=united states&api_key='+ api_key_lastfm +'&format=json');
         if(!res) {
             throw new Error("Something went wrong!");
         }
         return res.json();
     },
-    async getCover(mbid){
-        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='+ api_key +'&mbid=' + mbid +'&format=json');
+    async getTrack(mbid){
+        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='+ api_key_lastfm +'&mbid=' + mbid +'&format=json');
+        if(!res) {
+            throw new Error("Something went wrong!");
+        }
+        return res.json();
+    },
+    async getTopArtists(){
+        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key='+ api_key_lastfm +'&format=json');
+        if(!res) {
+            throw new Error("Something went wrong!");
+        }
+        return res.json();
+    },
+    async getArtist(mbid){
+        let res = await fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&api_key='+ api_key_lastfm +'&mbid=' + mbid +'&format=json');
         if(!res) {
             throw new Error("Something went wrong!");
         }

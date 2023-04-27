eba03f3a14e313efe3e18663fd5a7803fffaf115
Greg0s <gregoire.tinnes@outlook.fr>
2023-04-27 18:20:14 +0200

    styling, add confirm button

--- src/components/QuizAnswerField.vue
+++ src/components/QuizAnswerField.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="fields">
-        <input @keyup.enter="checkAnswer" id="textField" v-model="attempt"/>
+        <div class="textfield-box">
+            <input @keyup.enter="checkAnswer" placeholder="Type answer here" id="textField" v-model="attempt"/>
+            <span id="white-bg"></span>
+            <svg @click="checkAnswer" id="arrow" viewBox="0 0 18 6" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.7652 3.26517C17.9116 3.11872 17.9116 2.88128 17.7652 2.73483L15.3787 0.34835C15.2322 0.201903 14.9948 0.201903 14.8483 0.34835C14.7019 0.494796 14.7019 0.732233 14.8483 0.87868L16.9697 3L14.8483 5.12132C14.7019 5.26777 14.7019 5.5052 14.8483 5.65165C14.9948 5.7981 15.2322 5.7981 15.3787 5.65165L17.7652 3.26517ZM0 3.375H17.5V2.625H0V3.375Z"/>
+            </svg>
+        </div>
         <button class="skip" @click="skip">Skip</button>
     </div>
 </template>
@@ -23,7 +29,9 @@ export default{
             this.clear();
         },
         checkAnswer(){
-            this.toApp(this.attempt);
+            if(this.attempt != ""){
+                this.toApp(this.attempt);
+            }
         },
         skip(){
             this.$emit("skip");
@@ -40,9 +48,31 @@ export default{
         width: auto;
         display: block;
         margin: auto;
-        border: 1px solid grey;
+        border: none;
+        /* border: 1px solid var(--text-primary-color); */
+        width: 80%;
+        background: transparent;
+        /* margin-bottom: 1rem; */
+        color: var(--text-primary-color);
+    }
+    #textField:focus{
+        outline: none;
+    }
+    .textfield-box{
+        position: relative;
+    }
+    #white-bg{
+        /* border: 1px solid var(--text-primary-color); */
+        outline: 1px solid var(--text-primary-color);  /* inner stroke */
+        outline-offset: -1px;  /* negative border width + outline width */
+        text-align: start;
         width: 100%;
-        margin-bottom: 1rem;
+        height: 10rem;
+        position: absolute;
+        z-index: -1;
+        bottom: 0;
+        border-radius: 25px;
+        display: block;
     }
     .fields{
         width:100%;
@@ -57,5 +87,27 @@ export default{
         border : 1px solid var(--text-primary-color);
         font-family: 'Inter', sans-serif;
         height: 2rem;
+        margin-top: 1rem;
+        cursor: pointer;
+    }
+    button.skip:hover{
+        background-color: var(--accent-color);
+        color: var(--text-primary-color);
+        border:none;
+    }
+    #arrow{
+        fill: var(--text-primary-color);
+        width: 30px;
+        position: absolute;
+        right: 1.5rem;
+        bottom: 0.7rem;
+        background-color: var(--background-color-primary);
+        height: 1rem;
+        border-radius: 0;
+        cursor: pointer;
+        transition: transform .2s ease-in-out;
+    }
+    #arrow:hover{
+        transform: translateX(5px);
     }
 </style>

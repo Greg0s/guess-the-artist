bbf24c0089ec0f83de4c16c2b679a33a4a60f8b1
Greg0s <gregoire.tinnes@outlook.fr>
2023-05-24 21:33:00 +0200

    add resetScore btn

---
+++ src/components/QuizReset.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn reset" @click="reset">Reset score</button>
+</template>
+
+<script>
+export default {
+  name: 'QuizReset', 
+  methods: {
+    reset(){
+            this.$emit("reset");
+    }
+  }
+}
+</script>
+
+<style>
+  /*~~~~~~~~~~~~~~~~~~~~Artist image~~~~~~~~~~~~~~~~~~~~~~~*/
+  .reset{
+    width: 8rem;
+    margin: auto;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .reset{
+      margin-top: 1rem;
+    }
+  }
+</style>
